4083f0f1635f74bd6678d22d8c07a22ac257e9fa
Michael Bauer <michael@claimfound.com>
2020-09-17 20:19:11 -0400

    Nav and splashpage with animations work on any device

--- frontend/src/components/SplashPage/SplashPage.tsx
+++ frontend/src/components/SplashPage/SplashPage.tsx
@@ -9,8 +9,9 @@ interface SplashPageProps {
 export const SplashPage: FC<SplashPageProps> = () => {
     return (
         <div className="splash-page-container">
-            <div className="name-container"><h1>Mike</h1><h1>Bauer</h1></div>
-            <h2>Full Stack Developer</h2>
+            <div className="name-container"><h1 className="first-name">Mike</h1><h1 className="last-name">Bauer</h1></div>
+            <h2 className="tag">Full Stack Developer</h2>
+            <button className="enter-button"><a href="/home">Lets-a Go</a></button>
         </div >
     )
 }
