9027cd29377ddf77dd3bf7cdcb5e93fd28fb47cc
Michael Bauer <michael@claimfound.com>
2020-09-18 13:06:15 -0400

    Added home component

--- frontend/src/components/Home/Home.tsx
+++ frontend/src/components/Home/Home.tsx
@@ -1,5 +1,7 @@
 import React, { FC } from 'react'
 import './Home.css'
+import flower from './flower.gif'
+
 // import { HomeProps } from './Backdrop.Interface'
 
 interface HomeProps {
@@ -8,7 +10,19 @@ interface HomeProps {
 
 export const Home: FC<HomeProps> = () => {
     return (
-        <div className="home-container">Home</div>
+        <div className="home-container">
+            <div className="home-container-content">
+                <div className="top-container">
+                    <h1>You found me.</h1>
+                    <img src={flower} alt="Loading..." />
+                </div>
+                <div className="bottom-container">
+                    <p>I'm glad you're here. This is a <span>full stack</span> web app demonstrating my passion and skills for software development.
+                    It features a <span>React</span> frontend, a <span>Python Flask-Restful</span> backend api utilizing a <span>Flask SQLAlchemy ORM</span> to interact with its database, and an <span>Nginx</span> web server to connect the two together.
+                    The whole app is <span>Dockerized</span> in <span>Ubuntu 18.04</span> and both the development start-up and deployment to <span>AWS</span> is fully automated by <span>bash</span> scripts. You can view the source code <a href="https://github.com/michaelbauerinc/portfolio" rel="noopener noreferrer" target="_blank"><span>here.</span></a></p>
+                </div>
+            </div>
+        </div>
     )
 }
 
