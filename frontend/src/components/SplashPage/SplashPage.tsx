e21135f99f659c9224946348bdf40bda695aa6ee
Michael Bauer <michael@claimfound.com>
2020-09-17 20:32:30 -0400

    fix button on splash page

--- frontend/src/components/SplashPage/SplashPage.tsx
+++ frontend/src/components/SplashPage/SplashPage.tsx
@@ -1,5 +1,7 @@
 import React, { FC } from 'react'
 import './SplashPage.css'
+import { Link } from 'react-router-dom'
+
 // import { HomeProps } from './Backdrop.Interface'
 
 interface SplashPageProps {
@@ -11,7 +13,9 @@ export const SplashPage: FC<SplashPageProps> = () => {
         <div className="splash-page-container">
             <div className="name-container"><h1 className="first-name">Mike</h1><h1 className="last-name">Bauer</h1></div>
             <h2 className="tag">Full Stack Developer</h2>
-            <button className="enter-button"><a href="/home">Lets-a Go</a></button>
+            <Link to='/home'>
+                <button className="enter-button">Lets-a Go</button>
+            </Link>
         </div >
     )
 }
