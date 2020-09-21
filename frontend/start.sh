b6ed69882e80ec5cbace4609641bf2b83486aad8
Michael Bauer <michael@claimfound.com>
2020-09-21 14:00:33 -0400

    Added production automation for frontend

--- frontend/start.sh
+++ frontend/start.sh
@@ -1,3 +1,17 @@
 #!/bin/bash
-echo "Starting frontend"
-npm start
+
+echo "Starting frontend with environment type $ENVIRONMENT_TYPE"
+
+if [ $ENVIRONMENT_TYPE = "production" ]; then
+    if [ ! -e "./build" ]; then
+        echo "Creating production build of frontend."
+        npm run build
+    else
+        echo "Found production build of frontend, no need to create."
+    fi
+    echo "Starting production server"
+    npm run start-prod
+else 
+    echo "Starting frontend development server"
+    npm start
+fi
