b867e2496c8be0503dd6fa8e0be88299b4ca2ac7
Michael Bauer <michael@claimfound.com>
2020-09-23 20:30:47 -0400

    attempt poster fix for ios bug

--- frontend/src/components/Background/Background.tsx
+++ frontend/src/components/Background/Background.tsx
@@ -2,14 +2,14 @@ import React, { FC } from 'react'
 import './Background.css'
 // import { BackgroundProps } from './Background.Interface'
 import background from './background.mp4';
-
+import poster from './background.png'
 interface BackgroundProps {
 
 }
 
 export const Background: FC<BackgroundProps> = () => {
     return (
-        <video className="background-video" autoPlay={true} muted={true} loop={true} playsInline={true} preload="auto">
+        <video className="background-video" autoPlay={true} muted={true} loop={true} playsInline={true} preload="auto" poster={poster}>
             <source src={background} type="video/mp4" />
             <source src={background} type="video/ogg" />
     Your browser does not support the video tag.
