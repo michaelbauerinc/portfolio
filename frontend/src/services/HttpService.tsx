63fc64c524c796c80e831cf78d8cb80696a788ed
Michael Bauer <michael@claimfound.com>
2020-09-25 17:49:31 -0400

    fix connection prefix in HttpService

--- frontend/src/services/HttpService.tsx
+++ frontend/src/services/HttpService.tsx
@@ -2,9 +2,10 @@ import axios, { AxiosResponse } from 'axios';
 
 export class HttpService {
     private static BE_DOMAIN = process.env.REACT_APP_BE_DOMAIN;
+    private static prefix = (HttpService.BE_DOMAIN === "api.localhost") ? "http://" : "https://"
     private static axios = axios.create({
         // .. where we make our configurations
-        baseURL: `http://${HttpService.BE_DOMAIN}`
+        baseURL: `${HttpService.prefix}${HttpService.BE_DOMAIN}`
     });
     static async post(endpoint: string, data: Record<string, string>) {
         try {
@@ -15,6 +16,7 @@ export class HttpService {
         }
     }
     static async get(endpoint: string, data: Record<string, string> | Record<string, boolean>) {
+        console.log(HttpService.BE_DOMAIN)
         try {
             let response: AxiosResponse = await this.axios.get(endpoint, { params: data })
             return response
